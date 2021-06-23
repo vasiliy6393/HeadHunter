25ceebbeb460a3db09dc6a3acbe337fa2521ab99
Vasiliy <vasiliy.6393@gmail.com>
2021-06-23 21:32:47 +0500

    Update hh.sh

--- hh.sh
+++ hh.sh
@@ -10,7 +10,7 @@ DATE="$(which date)";
 HEAD="$(which head)";
 TAIL="$(which tail)";
 TELEGRAM_SEND="$(which telegram_send.sh)";
-LOG_FILE="/var/log/hh.log";
+LOG="/var/log/hh.log";
 HH_CODE="$(cat /var/log/hh.code)"; # код генерируется другим скриптом
 URL="https://api.hh.ru/resumes";
 
@@ -30,24 +30,24 @@ $CURL -s -H "Authorization: Bearer $HH_CODE" "$URL/mine" |
         # обновит резюме если прошло больше 4 часов с последнего обновления
         if [[ "$WAIT_TIME" -le "0" ]]; then
             if [[ "$n" == "0" ]]; then
-                $DATE > "$LOG_FILE"; n="1";
+                $DATE > "$LOG"; n="1";
             fi
             id="$(echo "$line" | $AWK -F, '{print $1}')";
             title="$(echo "$line" | $AWK -F, '{print $2}')";
             UPDATE="$($CURL --request POST -si -H "Authorization: Bearer $HH_CODE" \
                                                                                      "$URL/$id/publish")";
             if echo "$UPDATE" | $GREP -Pq 'HTTP/2 20[0-9]'; then
-                echo "Резюме $title успешно обновлено" >> "$LOG_FILE";
+                echo "Резюме \"$title\" успешно обновлено" >> "$LOG";
             elif echo "$UPDATE" | $GREP -Pq 'HTTP/2 403'; then
                 ERROR="Требуется авторизация";
                 ERROR_LOWER="$(echo "$ERROR" | $AWK '{print tolower($0)}')";
                 # оповещение меня с помощью Telegram-бота
                 $TELEGRAM_SEND "hh.sh: $ERROR_LOWER" > /dev/null 2>&1;
-                echo "$ERROR" >> "$LOG_FILE";
+                echo "$ERROR" >> "$LOG";
                 break;
             else
-                echo "$UPDATE" | $GREP -P '^HTTP\/2' | $HEAD -c-2 >> "$LOG_FILE";
-                echo "- $UPDATE" | $TAIL -n1 | $AWK -F\" '{print $4}' >> "$LOG_FILE";
+                echo "$UPDATE" | $GREP -P '^HTTP\/2' | $HEAD -c-2 >> "$LOG";
+                echo "- $UPDATE" | $TAIL -n1 | $AWK -F\" '{print $4}' >> "$LOG";
             fi
         fi
     done
