b0396db7ba4b68ffc9b55d40dd1bf52641ee1b58
Vasiliy <vasiliy.6393@gmail.com>
2021-06-25 04:17:44 +0500

    Update hh.sh

--- hh.sh
+++ hh.sh
@@ -1,53 +1,55 @@
 #!/bin/sh
 
-n="0";
-JQ="jq --compact-output";
-CURL="$(which curl)";
-SED="$(which sed)";
-GREP="$(which grep)";
-AWK="$(which awk)";
-DATE="$(which date)";
-HEAD="$(which head)";
-TAIL="$(which tail)";
-TELEGRAM_SEND="$(which telegram_send.sh)";
-LOG="/var/log/hh.log";
-HH_CODE="$(cat /var/log/hh.code)"; # код генерируется другим скриптом
-URL="https://api.hh.ru/resumes";
+export JQ="jq --compact-output";
+export CAT="$(which cat)";
+export AWK="$(which awk)";
+export SED="$(which sed)";
+export GREP="$(which grep)";
+export TAIL="$(which tail)";
+export HEAD="$(which head)";
+export DATE="$(which date)";
+export CURL="$(which curl)";
+export KILL="$(which kill)";
+export KILLALL="$(which killall)";
+export TELEGRAM_SEND="$(which telegram_send.sh)";
+export URL="https://api.hh.ru/resumes";
+export CODE="$(cat /tmp/hh.code)"; # код генерируется другим скриптом
+export LOG="/var/log/hh.log";
 
-$CURL -s -H "Authorization: Bearer $HH_CODE" "$URL/mine" |
-    $JQ ".items[] | {id, title}, .access.type.name" | $SED ':a;N;$!ba;s/}\n"/} "/g' |
+# если будет запущен второй экземпляр - первый будет убит
+PID_FILE='/tmp/hh.pid';
+if [[ -e "$PID_FILE" ]]; then
+    LAST_PID="$($CAT "$PID_FILE")";
+    [[ "$LAST_PID" =~ ^[0-9]+$ ]] && $KILL -9 $LAST_PID;
+fi
+echo "$$" > "$PID_FILE";
+
+function _update(){
+    title="$1";
+    RES_UPDATE="$($CURL --request POST -si -H "Authorization: Bearer $CODE" \
+                                          "$URL/$id/publish")";
+    if echo "$RES_UPDATE" | $GREP -Pq 'HTTP/2 20[0-9]'; then
+        echo "Резюме \"$title\" успешно обновлено" >> "$LOG";
+    elif echo "$RES_UPDATE" | $GREP -Pq 'HTTP/2 403'; then
+        ERROR="Требуется авторизация";
+        ERROR_LOWER="$(echo "$ERROR" | $AWK '{print tolower($0)}')";
+        # оповещение меня с помощью Telegram-бота
+        $TELEGRAM_SEND "hh.sh: $ERROR_LOWER" > /dev/null 2>&1;
+        echo "$ERROR" >> "$LOG";
+    else
+        ERROR="$(echo "$RES_UPDATE" | $TAIL -n1 | $AWK -F\" '{print $10}')";
+        echo "$ERROR - $title" >> "$LOG";
+    fi
+}
+
+n="0";
+$CURL -s -H "Authorization: Bearer $CODE" "$URL/mine" |
+    $JQ ".items[] | {id, title}, .access.type.name" | 
+    $SED ':a;N;$!ba;s/}\n"/} "/g' |
     $GREP -Piv 'доступно только по прямой ссылке|не видно никому' |
     $AWK -F\" '{print $4","$8}' | while read line; do
         id="$(echo "$line" | $AWK -F, '{print $1}')";
         title="$(echo "$line" | $AWK -F, '{print $2}')";
-        NOW="$($DATE +%s)";
-        RESUME="$($CURL --request GET -s -H "Authorization: Bearer $HH_CODE" \
-                                           "$URL/$id")";
-        UPDATED_AT="$(echo "$RESUME" | $JQ '.updated_at' | $SED 's/\"//g')";
-        UPDATED_AT_TIMESTAMP="$($DATE -d "$UPDATED_AT" +"%s")";
-        DIFF_TIME_UPTADED_AT="$((($NOW-$UPDATED_AT_TIMESTAMP)))";
-        WAIT_TIME="$((14400-$DIFF_TIME_UPTADED_AT))";
-        # обновит резюме если прошло больше 4 часов с последнего обновления
-        if [[ "$WAIT_TIME" -le "0" ]]; then
-            if [[ "$n" == "0" ]]; then
-                $DATE > "$LOG"; n="1";
-            fi
-            id="$(echo "$line" | $AWK -F, '{print $1}')";
-            title="$(echo "$line" | $AWK -F, '{print $2}')";
-            UPDATE="$($CURL --request POST -si -H "Authorization: Bearer $HH_CODE" \
-                                                                                     "$URL/$id/publish")";
-            if echo "$UPDATE" | $GREP -Pq 'HTTP/2 20[0-9]'; then
-                echo "Резюме \"$title\" успешно обновлено" >> "$LOG";
-            elif echo "$UPDATE" | $GREP -Pq 'HTTP/2 403'; then
-                ERROR="Требуется авторизация";
-                ERROR_LOWER="$(echo "$ERROR" | $AWK '{print tolower($0)}')";
-                # оповещение меня с помощью Telegram-бота
-                $TELEGRAM_SEND "hh.sh: $ERROR_LOWER" > /dev/null 2>&1;
-                echo "$ERROR" >> "$LOG";
-                break;
-            else
-                echo "$UPDATE" | $GREP -P '^HTTP\/2' | $HEAD -c-2 >> "$LOG";
-                echo "- $UPDATE" | $TAIL -n1 | $AWK -F\" '{print $4}' >> "$LOG";
-            fi
-        fi
+        if [[ "$n" == "0" ]]; then $DATE > "$LOG"; n="1"; fi
+        _update "$title";
     done
